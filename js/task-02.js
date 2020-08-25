32ec654ac1176a56846ccbadaccc73bd067bfc94
Serhii Malyshko <malyshkosergiy@gmail.com>
2020-08-25 23:44:51 +0300

    task 2 is added

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const dish = ingredients.forEach(function (itemName) {
+    const ingredientsList = document.createElement('li');
+    ingredientsList.classList.add('ingredients__vegetables');
+    ingredientsList.textContent = itemName;
+    ingredientsRef.appendChild(ingredientsList);
+    console.log(ingredientsRef);
+});
