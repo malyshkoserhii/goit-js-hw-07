a111ef9e157018eb0a48e0162fad85dec5a38301
Serhii Malyshko <malyshkosergiy@gmail.com>
2020-08-27 23:57:09 +0300

    task 6 is added

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+const inputRef = document.querySelector('#name-input');
+const spanRef = document.querySelector('#name-output');
+const contentOfSpanFromMarkup = spanRef.textContent;
+
+const handleInputChangeName = (event) => {
+    spanRef.textContent = event.target.value;
+}
+
+const handleInputEmptyField = (event) => {
+    if (event.target.value === "") {
+        spanRef.textContent = contentOfSpanFromMarkup;
+    }
+}
+
+inputRef.addEventListener('input', handleInputChangeName);
+inputRef.addEventListener('blur', handleInputEmptyField);
