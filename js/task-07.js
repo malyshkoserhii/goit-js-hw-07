e2256b55049bf50ff4bdc3d836a1a61c21fda641
Serhii Malyshko <malyshkoserhii91@gmail.com>
2020-08-28 12:07:50 +0300

    task 7 is added

--- js/task-07.js
+++ js/task-07.js
@@ -1,14 +1,8 @@
-const inputRef = document.querySelector('#font-size-control');
+const inputRef = document.querySelector("#font-size-control");
 const textRef = document.querySelector("#text");
 
 const changeFontSize = (event) => {
-    console.log('valueAsNumber', event.target.valueAsNumber)
-    console.log('style.fontSize', Number.parseInt(textRef.style.fontSize))
-    console.log(textRef)
-    // event.target.value === Number.parseInt(textRef.style.fontSize);
+  textRef.style.fontSize = event.target.value + "px";
+};
 
-    // Number.parseInt(textRef.style.fontSize) += Number(event.target.valueAsNumber);
-
-}
-
-inputRef.addEventListener('input', changeFontSize);
+inputRef.addEventListener("input", changeFontSize);
