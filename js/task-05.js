12b4741095dbe43fabcca415b567cb6d59155a19
Serhii Malyshko <malyshkoserhii91@gmail.com>
2020-09-03 20:32:35 +0300

    task 5 has been fixed; task 8 has been started

--- js/task-05.js
+++ js/task-05.js
@@ -1,16 +1,16 @@
-const inputRef = document.querySelector('#name-input');
-const spanRef = document.querySelector('#name-output');
+const inputRef = document.querySelector("#name-input");
+const spanRef = document.querySelector("#name-output");
 const contentOfSpanFromMarkup = spanRef.textContent;
 
 const handleInputChangeName = (event) => {
-    spanRef.textContent = event.target.value;
-}
+  spanRef.textContent = event.target.value;
+};
 
 const handleInputEmptyField = (event) => {
-    if (event.target.value === "") {
-        spanRef.textContent = contentOfSpanFromMarkup;
-    }
-}
+  if (event.target.value === "") {
+    spanRef.textContent = contentOfSpanFromMarkup;
+  }
+};
 
-inputRef.addEventListener('input', handleInputChangeName);
-inputRef.addEventListener('blur', handleInputEmptyField);
+inputRef.addEventListener("input", handleInputChangeName);
+inputRef.addEventListener("input", handleInputEmptyField);
